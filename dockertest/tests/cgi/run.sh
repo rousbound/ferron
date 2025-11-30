be67a452472706a7ac5a0d7b301c33a048a2da8e
Geraldo Luiz <geraldoluiz@puc-rio.br>
2025-11-30 16:47:28 -0300

    test cgi with no shebang

--- dockertest/tests/cgi/run.sh
+++ dockertest/tests/cgi/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 TEST_FAILED=0
-docker compose --progress quiet up --build -d > /dev/null || (echo "Failed to start containers for a test suite" >&2; exit 1)
-cat test.sh | docker compose --progress quiet exec -T test-runner bash 2>&1 || TEST_FAILED=1
-docker compose --progress quiet kill -s SIGKILL > /dev/null || true
-docker compose --progress quiet down -v > /dev/null || true
+docker compose  up --build -d  || (echo "Failed to start containers for a test suite" >&2; exit 1)
+cat test.sh | docker compose  exec -T test-runner bash 2>&1 || TEST_FAILED=1
+docker compose  kill -s SIGKILL  || true
+docker compose  down -v  || true
 if [ "$TEST_FAILED" -eq 1 ]; then
     exit 1
 fi
--- dockertest/tests/cgi/test.sh
+++ dockertest/tests/cgi/test.sh
@@ -27,3 +27,21 @@ fi
 if [ "$TEST_FAILED" -eq 1 ]; then
     exit 1
 fi
+
+
+TEST_RESULTS="$(curl -fsSL http://ferron/cgi-bin/index_no_shebang.sh)"
+TEST_EXIT_CODE=$?
+TEST_EXPECTED="Hello, World!"
+if [ "$TEST_EXIT_CODE" -eq 0 ] && [ "$TEST_RESULTS" = "$TEST_EXPECTED" ]; then
+    echo "Basic CGI test passed!"
+else
+    echo "Basic CGI test failed!" >&2
+    echo "  Exit code: $TEST_EXIT_CODE" >&2
+    echo "  Expected: $TEST_EXPECTED" >&2
+    echo "  Received: $TEST_RESULTS" >&2
+    TEST_FAILED=1
+fi
+
+if [ "$TEST_FAILED" -eq 1 ]; then
+    exit 1
+fi
