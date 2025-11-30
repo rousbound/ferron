c79964add1b8e117fb021d89925f7d29f26a68a5
Geraldo Luiz <geraldoluiz@puc-rio.br>
2025-11-30 17:51:28 -0300

    improvements cgi-no-shebang test

--- dockertest/tests/cgi/test.sh
+++ dockertest/tests/cgi/test.sh
@@ -29,13 +29,13 @@ if [ "$TEST_FAILED" -eq 1 ]; then
 fi
 
 
-TEST_RESULTS="$(curl -fsSL http://ferron/cgi-bin/index_no_shebang.sh)"
+TEST_RESULTS="$(curl -fsSL http://ferron/scripts/index_no_shebang.sh)"
 TEST_EXIT_CODE=$?
 TEST_EXPECTED="Hello, World!"
 if [ "$TEST_EXIT_CODE" -eq 0 ] && [ "$TEST_RESULTS" = "$TEST_EXPECTED" ]; then
-    echo "Basic CGI test passed!"
+    echo "Basic CGI-no-shebang test passed!"
 else
-    echo "Basic CGI test failed!" >&2
+    echo "Basic CGI-no-shebang test failed!" >&2
     echo "  Exit code: $TEST_EXIT_CODE" >&2
     echo "  Expected: $TEST_EXPECTED" >&2
     echo "  Received: $TEST_RESULTS" >&2
